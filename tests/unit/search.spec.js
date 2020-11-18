205f98cf0cfe26d1cde8220b55185e80f7fb0d1e
hochong_desktop <45001307+hochong@users.noreply.github.com>
2020-11-18 23:00:15 +0800

    search bar test

--- tests/unit/search.spec.js
+++ tests/unit/search.spec.js
@@ -1,7 +1,5 @@
 import {shallowMount} from "@vue/test-utils";
 import Search from "@/components/Search.vue";
-import 'jest-localstorage-mock';
-
 
 describe("Chart.vue", () => {
     it("check input text", () => {
@@ -15,8 +13,8 @@ describe("Chart.vue", () => {
         const inputTextBar = wrapper.find("input");
         expect(inputTextBar.element.value).toMatch("US");
     });
-    it("check click button", () => {
-      /*
+    it("check click button", async () => {
+        const spyLocalStorage = jest.spyOn(Search.methods, "pressSearchButton");
         const wrapper = shallowMount(Search, {
             data: function() {
                 return {
@@ -25,12 +23,14 @@ describe("Chart.vue", () => {
             },
         });
         const searchButton = wrapper.find("button");
-        //console.log(wrapper);
-        //const spy = jest.spyOn(wrapper.methods, "setlocalStorageValue");
         searchButton.trigger("click");
-        //expect(spy).toHaveBeenCalled();
-        //spy.mockRestore();
-        expect(localStorage.setItem).toHaveBeenLastCalledWith("country","Canada");
+
+        expect(spyLocalStorage).toHaveBeenCalled();
+        /*
+        console.log(JSON.parse(global.localStorage.getItem("country")));
+        console.log(window.localStorage);
+        console.log(localStorage.getItem("key"));
         */
+        spyLocalStorage.mockReset();
     });
 });
